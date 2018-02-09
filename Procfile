fe99e0daaf6059878d6d88f51b67cd38ce3d9b5b
Wyatt Andersen <wandersen02@gmail.com>
2018-02-09 09:04:32 -0600

    Removing inet in dev and stage

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-release: rake db:migrate && rake db:migrat RAILS_ENV=test 
+release: rake db:migrate
--- db/schema.rb
+++ db/schema.rb
@@ -178,8 +178,8 @@ ActiveRecord::Schema.define(version: 20180209041809) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string     "current_sign_in_ip"
+    t.string     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
